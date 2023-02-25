d94c2995181f43e7e03d4056b75990cb0a95da27
Sawon_bhowmik <sawonbhowmik@gmail.com>
2023-02-26 01:12:40 +0530

    First commit

---
+++ src/test/java/com/project/kubernetes/SpringKubernetesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKubernetesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
