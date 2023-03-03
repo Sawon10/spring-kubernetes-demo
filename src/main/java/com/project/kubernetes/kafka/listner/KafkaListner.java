370c0697715e39d314cdf541f4bddae5deb6e458
Sawon_bhowmik <sawonbhowmik@gmail.com>
2023-03-03 12:25:56 +0530

    added log4j2 in pom

--- src/main/java/com/project/kubernetes/kafka/listner/KafkaListner.java
+++ src/main/java/com/project/kubernetes/kafka/listner/KafkaListner.java
@@ -1,13 +1,18 @@
 package com.project.kubernetes.kafka.listner;
 
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaListner {
 	
+	private static Logger LOGGER = LogManager.getLogManager().getLogger("KafkaListner");
 	@KafkaListener(topics = "Test-topic", groupId = "local")
 	public void listnerTestTopic(String message){
+		LOGGER.info("The message is : {}"+ message);
 		System.out.println(message);
 	}
 	
